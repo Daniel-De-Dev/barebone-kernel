762308944c769db08fdf3b8830bebf0e7cf74a01
Daniel-De-Dev <id@daniel-de-dev.com>
2026-08-09 01:41:42 +0200

    refactor: switched to sbi crate
    looking at this crate it looks more attractive. Also comes with no
    dependencies of its own

--- bootloader/src/logging/sbi.rs
+++ bootloader/src/logging/sbi.rs
@@ -5,7 +5,7 @@ pub(super) struct SbiConsole;
 impl fmt::Write for SbiConsole {
   fn write_str(&mut self, string: &str) -> fmt::Result {
     for byte in string.bytes() {
-      let ret = sbi_rt::console_write_byte(byte);
+      let ret = sbi::debug_console::write_byte(byte);
 
       if ret.is_err() {
         return Err(fmt::Error);
